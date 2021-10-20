8c2f0b2c4fb9cd6413a7afa2923402f92247bfc4
matteoiorio <iorio436@outlook.it>
2021-10-20 17:29:42 +0200

    bonus update

---
+++ bonus/JS/script.js
@@ -0,0 +1,32 @@
+/* 
+CREATE A "SECURE" PASSWORD BY FILLING A FEW PROMPTS.
+IT MUST BE MADE BY COMBINING NAME, SURNAME AND FAV COLOR
+AND THE RESULTS MUST BE THE ANSWER TO THOSE QUESTIONS
+*/
+
+//ask and register name
+const name= prompt("Qual è il tuo nome?");
+//console.log('il mio nome è: ' + name);
+
+//ask and register surname
+const surname= prompt("Qual è il tuo cognome?");
+//console.log('il mio cognome è: ' + surname);
+
+//ask and register fav color
+const favColor= prompt("Qual è il tuo colore preferito?");
+//console.log('il mio colore preferito è: ' + favColor);
+
+//give back (in page) generated password
+// document.getElementById ('secure').innerHTML= `La password assegnata è: ${name}${surname}${favColor}21`;
+
+/* 
+BONUS
+TRYING TO CREATE A VARIABLE
+THAT PRINTS THE CONCATENATED PASSWORD SAVED BEFORE
+*/
+
+// create a single variable that contains every prompt variable
+const password= name + surname + favColor + 21
+
+//print that variable
+document.getElementById ('secure').innerHTML=`La password assegnata è: ${password}`
